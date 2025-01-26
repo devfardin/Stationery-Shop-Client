b878208b71a4eba761306b23810a86c947033dd3
devfardin <contactfardin22@gmail.com>
2025-01-26 15:28:08 +0600

    redux hook

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -4,9 +4,14 @@ import { FieldValues } from 'react-hook-form'
 import STInput from '../../components/form/STInput'
 import SubmitBtn from '../../components/form/SubmitBtn'
 const Login = () => {
+const dispatch = useAppDis
     const onSubmit = async (data: FieldValues) => {
         console.log(data);
         
+    }
+    const defaultValue= {
+      email: 'john.doe@example.com',
+      password: 'passeword123',
     }
   return (
     <div className='mt-16 flex justify-center items-center'>
@@ -16,7 +21,7 @@ const Login = () => {
                 <h1 className='text-black text-3xl font-bold text-center mb-2'>Login</h1>
                 <p className='text-base font-normal text-neutral-500 text-center'>Please login using account detail bellow.</p>
             </div>
-            <STForm onSubmit={onSubmit} >
+            <STForm onSubmit={onSubmit} defaultValues={defaultValue}>
                 <STInput name='email' label='Account Email' type='email'/>
                 <STInput name='password' label='Account Password' type='password'/>
                 <SubmitBtn type='submit' label='Login'/>
